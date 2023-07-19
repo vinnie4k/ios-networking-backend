083daefbce1dc6cd79d5b469fd58e09fc1a87e62
Vin Bui <vdb23@cornell.edu>
2023-07-18 23:27:47 -0500

    Finished GET and POST routes

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,67 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// Firebase Admin SDK
+const serviceAccount = "./service_account_key.json";
+const admin = require("firebase-admin");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ios-networking-lec-default-rtdb.firebaseio.com",
+});
 
+const { getDatabase } = require("firebase-admin/database");
 const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// Express
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors({ origin: true }));
+app.use(express.json());
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+/**
+ * Fetch all members
+ */
+app.get("/", async (req, res) => {
+  try {
+    const snapshot = await getDatabase().ref("/").once("value");
+    return res.status(200).send(snapshot.val());
+  } catch (error) {
+    return res.status(400).send("Unable to fetch all members");
+  }
+});
+
+/**
+ * Add a member to the roster
+ *
+ * Request body contains:
+ * - name (String)
+ * - position (String)
+ * - subteam (String)
+ */
+app.post("/", async (req, res) => {
+  const name = req.body.name;
+  const position = req.body.position;
+  const subteam = req.body.subteam;
+
+  if (name == null || position == null || subteam == null) {
+    return res.status(400).send("Invalid request body");
+  }
+
+  try {
+    const snapshot = await getDatabase().ref("/").once("value");
+    const members = snapshot.val();
+
+    const newMember = {
+      name: name,
+      position: position,
+      subteam: subteam,
+    };
+
+    members.push(newMember);
+
+    await getDatabase().ref("/").set(members);
+    return res.status(200).send(newMember);
+  } catch (error) {
+    return res.status(400).send("Unable to add a member");
+  }
+});
+
+exports.iosCourse = onRequest({ region: "us-east1" }, app);
